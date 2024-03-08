e8530143d512713279ef49ba6944409bb65cd598
letunghust <tunglehoctot@gmail.com>
2024-03-08 20:07:38 +0700

    8/3 create dashboard

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -14,3 +14,11 @@ const Home = () => {
 }
 
 export default Home
+
+/**
+ * 8/3: 
+ *  - them 2 nút ấn vào trong phần home
+ *  - tạo thêm component slider để cho cả 2 phần đều chuyển động được
+ *   
+ * 
+ */
--- src/routers/router.jsx
+++ src/routers/router.jsx
@@ -5,7 +5,12 @@ import Home from "../home/Home";
 import About from "../components/About";
 import Blog from "../components/Blog";
 import SingleBookHome from "../shop/SingleBookHome";
-  
+import DashboardLayout from "../dashboard/DashboardLayout";
+import Dashboard from "../dashboard/Dashboard";
+import UploadBook from "../dashboard/UploadBook";
+import ManageBooks from "../dashboard/ManageBooks";  
+import EditBooks from "../dashboard/EditBooks"
+
   const router = createBrowserRouter([
     {
       path: "/",
@@ -29,8 +34,29 @@ import SingleBookHome from "../shop/SingleBookHome";
         },
         {
           path: "/book/:id",
-          element: <SingleBookHome/>
-         
+          element: <SingleBookHome/>   
+        },
+        {
+          path: "/admin/dashboard",
+          element: <DashboardLayout/>,
+          children: [
+            {
+              path: "/admin/dashboard",
+              element: <Dashboard/>
+            },
+            {
+              path: "/admin/dashboard/upload",
+              element: <UploadBook/>
+            },
+            {
+              path: "/admin/dashboard/manage",
+              element: <ManageBooks/>
+            },
+            {
+              path: "/admin/dashboard/edit-books/:id",
+              element: <EditBooks/>
+            }
+          ]
         }
       ]
     },
