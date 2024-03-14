62cb000645f20106d29f82a4bbabc1906e23281c
letunghust <tunglehoctot@gmail.com>
2024-03-14 17:53:18 +0700

    14/3 update banner

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,11 +1,11 @@
 
 const Banner = () => {
   return (
-    <div className="flex justify-between items-center p-8 bg-gray-200">
+    <div className="flex justify-center items-center p-8 bg-gray-200">
       {/* left side bar */}
       <div className="text-lg font-semibold text-blue-500"> 
         <div className="text-center">
-          <h2 className="text-5xl font-bold mb-4">Buy and sell your books</h2>
+          <h2 className="text-5xl font-bold mb-4">Search books</h2>
           <div className="flex items-center">
             <input
               className="border border-gray-300 px-4 py-2 rounded-l-full focus:outline-none"
@@ -21,7 +21,7 @@ const Banner = () => {
         </div>
       </div>
       {/* right side bar */}
-      <div className="text-lg font-semibold text-red-500">Right Side</div>
+      {/* <div className="text-lg font-semibold text-red-500">Right Side</div> */}
     </div>
   );
 };
