cf4910e3b79cb1db019bd83f1bd61e683d7975c5
letunghust <tunglehoctot@gmail.com>
2024-04-03 18:13:17 +0700

    3/4 v3 update display full book in cart

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -17,10 +17,12 @@ const Cart = () => {
                 },
               });
         
-              const data = response;
               console.log('data', response.data.books)
-            setCartItems(response.data.books[0].book); 
-            console.log(cartItems)
+            // setCartItems(response.data.books[0].book); 
+            setCartItems(response.data.books)
+            // console.log(cartItems[1].book._id)
+            
+
         } catch (error) {
             console.log('Lỗi khi lấy thông tin giỏ hàng:', error);
         }
@@ -31,24 +33,35 @@ const Cart = () => {
     }, []); 
 
     return (
-        <div>
-            <h2>Giỏ hàng của bạn</h2>
+        <div className="bg-white shadow-md rounded-lg p-4">
+            <h2 className="text-xl font-bold mb-4">Giỏ hàng của bạn</h2>
             {cartItems.length === 0 ? (
-                <p>Giỏ hàng của bạn đang trống.</p>
+                <p className='text-gray-600'>Giỏ hàng của bạn đang trống.</p>
             ) : (
-                <ul>
-                    <div>{cartItems.bookTitle}</div>
-                    <div>{cartItems.quantity}</div>
-                    <div>{cartItems.price}</div>
-                    {/* {cartItems.map((item, index) => (
-                        <li key={index}>
-                            <div>
-                                <span>{cartItems.bookTitle}</span>
-                                <span>Số lượng: {cartItems.quantity}</span>
-                                <span>Giá: ${cartItems.price}</span>
-                            </div>
+                <ul className="divide-y divide-gray-200 justify-between"> 
+                     {cartItems.map((item) => (
+                        <li key={item.book._id} className="py-4 flex  items-center">
+                        <img
+                            src={item.book.imageURL}
+                            alt={item.book.bookTitle}
+                            className="w-16 h-20 object-cover mr-4"
+                        />
+                        <div>
+                            <h3 className="text-lg font-semibold">{item.book.bookTitle}</h3>
+                            <p className="text-gray-600">Quantity: {item.quantity}</p>
+                            <p className="text-gray-600">Price: {item.book.price}$</p>
+                        </div>
                         </li>
-                    ))} */}
+                    ))}
+                    {/* <img 
+                        src={cartItems.imageURL}  
+                        alt={cartItems.bookTitle}
+                        className="w-16 h-20 object-cover mr-4"
+                    />
+                    
+                    <h3 className="text-lg font-semibold">{cartItems.bookTitle}</h3>
+                    <p className='text-gray-600'>{cartItems.quantity}</p>
+                    <p className='text-gray-600'>{cartItems.price}$</p> */}
                 </ul>
             )}
         </div>
