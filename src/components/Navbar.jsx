88d5bd247c2aceb07d252818231e7c439baf4be9
letunghust <tunglehoctot@gmail.com>
2024-04-17 11:28:36 +0700

    17_3 v1 create icon profile

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,44 +6,51 @@ import "./NavBar.css";
 import { BsCart } from "react-icons/bs";
 import { ThemeContext } from "../App";
 // import LoginForm from "../pages/LoginForm";
-import {jwtDecode}  from "jwt-decode";
+import { jwtDecode } from "jwt-decode";
 import axios from "axios";
 
 const Navbar = () => {
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  // const [isMenuOpen, setIsMenuOpen] = useState(false); 
   const [isSticky, setIsSticky] = useState(false);
   const { darkTheme, toggleTheme } = useContext(ThemeContext);
   let [isLogedIn, setIsLogedIn] = useState(false);
+  const [showPopup, setShowPopUp] = useState(false);
   const [userRole, setUserRole] = useState("");
   const navigate = useNavigate();
+  const defaultAvatar = "https://upload.wikimedia.org/wikipedia/commons/thumb/2/21/Flag_of_Vietnam.svg/1280px-Flag_of_Vietnam.svg.png";
   // const toggleMenu = () => {
   //     setIsMenuOpen(!isMenuOpen);
   // }
 
-  // get info from local storage to get role 
+  // get info from local storage to get role
   useEffect(() => {
     const storedToken = localStorage.getItem("token");
     if (storedToken) {
+      setIsLogedIn(true);
       try {
         const decodedToken = jwtDecode(storedToken);
         // console.log(decodedToken._id)
         const userId = decodedToken._id;
 
         const fetchUserData = async () => {
-            try {
-                const response = await axios.get(`${import.meta.env.VITE_BACKEND_URL}/user/${userId}`);
-                // console.log(response)
-                // console.log('data' ,response.data.role)
-                setUserRole(response.data.role);
-            } catch(error) {
-                console.log(error);
-            }
-        }
+          try {
+            const response = await axios.get(
+              `${import.meta.env.VITE_BACKEND_URL}/user/${userId}`
+            );
+            // console.log(response)
+            // console.log('data' ,response.data.role)
+            setUserRole(response.data.role);
+          } catch (error) {
+            console.log(error);
+          }
+        };
         fetchUserData();
       } catch (error) {
         console.error("Error decoding token from localStorage:", error);
         // Xử lý lỗi hoặc đặt giá trị mặc định cho userRole
       }
+    } else {
+      setIsLogedIn(false);
     }
   }, []);
 
@@ -70,15 +77,11 @@ const Navbar = () => {
     };
   }, []);
 
-  // navItems here
-  // const navItems = [
-  //     {link: "Home", path: "/"},
-  //     {link: "About", path: "/about"},
-  //     {link: "Shop", path: "/shop"},
-  //     // {link: "Admin", path: "/admin"},
-  //     userRole === 'admin' ? {link: "Admin", path: "/admin"} : null,
-  //     {link: "Blog", path: "/blog"},
-  // ]
+  // open pop up when click avatar
+  const togglePopup = () => {
+    setShowPopUp(!showPopup);
+  }
+
   const navItems = [
     { link: "Home", path: "/" },
     { link: "About", path: "/about" },
@@ -86,6 +89,7 @@ const Navbar = () => {
     { link: "Admin", path: "/admin" },
     { link: "Blog", path: "/blog" },
   ].filter((item) => item.link !== "Admin" || userRole === "admin");
+
   return (
     <header>
       <nav className={`flex justify-between`}>
@@ -105,18 +109,18 @@ const Navbar = () => {
           </ul>
         </div>
         <div className="flex">
+          {/* change light and dark theme */}
           <FaAffiliatetheme
             style={{ fontSize: "2rem" }}
             onClick={toggleTheme}
           />
+          {/* link to cart */}
           <Link to="/cart">
             <BsCart className="text-2xl" />
           </Link>
-          {isLogedIn ? (
+          {/* {isLogedIn ? (
             <button onClick={handleLogout}> Logout </button>
           ) : (
-            // <LoginForm setIsLogedIn={setIsLogedIn} />
-
             <Link to="/login">
               <button
                 onClick={() => {
@@ -126,6 +130,25 @@ const Navbar = () => {
               >
                 Login
               </button>
+            </Link>
+          )} */}
+          {isLogedIn ? (
+            <div>
+              <img src={defaultAvatar} alt="Avatar" onClick={togglePopup}  className="w-10 h-10 rounded-full cursor-pointer"/>
+              {showPopup ? (
+               <div className="absolute right-0 mt-2 bg-white rounded-md shadow-lg z-10">
+                  <Link to='/profile'  className="block px-4 py-2 text-gray-800 hover:bg-gray-100"> 
+                      Your profile
+                  </Link>
+                  <button onClick={handleLogout}  className="block w-full text-left px-4 py-2 text-gray-800 hover:bg-gray-100"> Logout </button>
+                </div>
+              ) : (
+                <></>
+              )}
+            </div>
+          ) : (
+            <Link to="/login"  className="text-white bg-blue-500 hover:bg-blue-600 px-4 py-2 rounded">
+             Login
             </Link>
           )}
         </div>
--- src/routers/router.jsx
+++ src/routers/router.jsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter} from "react-router-dom";
+import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
 import Shop from "../shop/Shop";
 import Home from "../home/Home";
@@ -8,85 +8,88 @@ import SingleBookHome from "../shop/SingleBookHome";
 import DashboardLayout from "../dashboard/DashboardLayout";
 import Dashboard from "../dashboard/Dashboard";
 import UploadBook from "../dashboard/UploadBook";
-import ManageBooks from "../dashboard/ManageBooks";  
-import EditBooks from "../dashboard/EditBooks"
+import ManageBooks from "../dashboard/ManageBooks";
+import EditBooks from "../dashboard/EditBooks";
 import LoginForm from "../pages/LoginForm";
 import SignUpForm from "../pages/SignUpForm";
 import Cart from "../pages/Cart";
 import { loadStripe } from "@stripe/stripe-js";
 import { Elements } from "@stripe/react-stripe-js";
-const stripePromise = loadStripe('pk_test_51P1hnME80pxaWCvIsuSE6XO7LLLuoWmZKjvuXDRD99v10auWXzuvCKHKtidf7tLUcSczK806Pvbt2PwRjUL6L3Jk00sNLNslNz');
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <App/>,
-      children: [
-        {
-            path: "/",
-            element: <Home/>
-        },
-        {
-            path: "/shop",
-            element: <Shop/>
-        },
-        {
-          path: "/about",
-          element: <About/>
-        },
-        {
-          path: "/blog",
-          element: <Blog/>
-        },
-        {
-          path: "/book/:id",
-          element: <SingleBookHome/>   
-        },
-        {
-          path: "/login",
-          element: <LoginForm/>
-        },
-        {
-          path: "/signup",
-          element: <SignUpForm/>
-        },
-        {
-          path: "/cart",
-          element:
-              <Elements
-      stripe={stripePromise}
-      // options={{
-      //   clientSecret:
-      //     "pi_3P3WUKE80pxaWCvI0rce7moD_secret_wbNwhXJj7LarsJaYScKpdS5GE",
-      //   elements: { cardElement: CardElement },
-      // }}
-    >
-          <Cart/> 
-           </Elements> 
-        },
-        {
-          path: "/admin",
-          element: <DashboardLayout/>,
-          children: [
-            {
-              path: "/admin",
-              element: <Dashboard/>
-            },
-            {
-              path: "/admin/upload",
-              element: <UploadBook/>
-            },
-            {
-              path: "/admin/manage",
-              element: <ManageBooks/>
-            },
-            {
-              path: "/admin/edit-books/:id",
-              element: <EditBooks/>
-            }
-          ]
-        }
-      ]
-    },
-  ]);
+import UpdateProfile from "../pages/UpdateProfile";
+const stripePromise = loadStripe(
+  "pk_test_51P1hnME80pxaWCvIsuSE6XO7LLLuoWmZKjvuXDRD99v10auWXzuvCKHKtidf7tLUcSczK806Pvbt2PwRjUL6L3Jk00sNLNslNz"
+);
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/shop",
+        element: <Shop />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/blog",
+        element: <Blog />,
+      },
+      {
+        path: "/book/:id",
+        element: <SingleBookHome />,
+      },
+      {
+        path: "/login",
+        element: <LoginForm />,
+      },
+      {
+        path: "/signup",
+        element: <SignUpForm />,
+      },
+      {
+        path: "/cart",
+        element: (
+          <Elements
+            stripe={stripePromise}
+          >
+            <Cart />
+          </Elements>
+        ),
+      },
+      {
+        path: "/profile",
+        element: <UpdateProfile/> 
+      },
+      {
+        path: "/admin",
+        element: <DashboardLayout />,
+        children: [
+          {
+            path: "/admin",
+            element: <Dashboard />,
+          },
+          {
+            path: "/admin/upload",
+            element: <UploadBook />,
+          },
+          {
+            path: "/admin/manage",
+            element: <ManageBooks />,
+          },
+          {
+            path: "/admin/edit-books/:id",
+            element: <EditBooks />,
+          },
+        ],
+      },
+    ],
+  },
+]);
 
-  export default router;
+export default router;
