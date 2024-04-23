980264c7ac0b29064e7e27173b91bf4ebdb56d22
letunghust <tunglehoctot@gmail.com>
2024-04-23 16:33:30 +0700

    23_4 v1 create swiper slider in home

--- src/components/BookCard.jsx
+++ src/components/BookCard.jsx
@@ -1,65 +1,61 @@
 /* eslint-disable react/prop-types */
 import React, { useRef, useState } from "react";
 import { Link } from "react-router-dom";
-import FavoriteBooks from "../home/FavoriteBooks";
+// import FavoriteBooks from "../home/FavoriteBooks";
 // Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
 
 // Import Swiper styles
 import "swiper/css";
 import "swiper/css/pagination";
+import "swiper/css/navigation"; // Thêm import cho CSS navigation
+import { Navigation, Pagination } from "swiper/modules";
 
-// import "./styles.css";
+const BookCard = ({ headline, books }) => {
+  return (
+    <div className="my-8 mx-auto max-w-2xl">
+      <h2 className="text-3xl font-bold mb-6">{headline}</h2>
+      <Swiper
+        slidesPerView={1}
+        spaceBetween={10}
+        pagination={{
+          clickable: true,
+        }}
+        navigation={true} // Thêm tham số navigation để hiển thị nút điều hướng
+        modules={[Pagination, Navigation]} // Thêm module Navigation
+        breakpoints={{
+          640: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+          },
+          768: {
+            slidesPerView: 4,
+            spaceBetween: 40,
+          },
+          1024: {
+            slidesPerView: 5,
+            spaceBetween: 50,
+          },
+        }}
+        className="mySwiper"
+      >
+        {books.map((book) => (
+          <SwiperSlide key={book._id}>
+            <Link to={`/book/${book._id}`}>
+              <div className="relative overflow-hidden aspect-ratio-2/3 rounded-lg">
+                <img
+                  src={book.imageURL}
+                  alt=""
+                  className="object-cover w-full h-full"
+                />
+              </div>
+              <div className="mt-2 text-sm font-medium">{book.bookTitle}</div>
+            </Link>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
+  );
+};
 
-// import required modules
-import { Navigation, Pagination } from 'swiper/modules';
-
-const BookCard = ({headline, books }) => {
-    return (
-      <div className="my-8 mx-auto max-w-2xl">
-        <h2 className="text-3xl font-bold mb-6">{headline}</h2>
-        <Swiper
-          slidesPerView={1}
-          spaceBetween={10}
-          pagination={{
-            clickable: true,
-          }}
-          breakpoints={{
-            640: {
-              slidesPerView: 2,
-              spaceBetween: 20,
-            },
-            768: {
-              slidesPerView: 4,
-              spaceBetween: 40,
-            },
-            1024: {
-              slidesPerView: 5,
-              spaceBetween: 50,
-            },
-          }}
-          modules={[Pagination]}
-          className="mySwiper"
-        >
-          {books.map((book) => (
-            <SwiperSlide key={book._id}>
-                <Link to={`/book/${book._id}`}>
-                  <div className="relative overflow-hidden aspect-ratio-2/3 rounded-lg">
-                    <img
-                      src={book.imageURL}
-                      alt=""
-                      className="object-cover w-full h-full"
-                    />
-                  </div>
-                  <div className="mt-2 text-sm font-medium">
-                    {book.bookTitle}
-                  </div>
-              </Link>
-            </SwiperSlide>
-          ))}
-        </Swiper>
-      </div>
-    );
-  };
-
-export default BookCard
+export default BookCard;
