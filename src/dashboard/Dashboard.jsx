0b614f625269de4378710098bdfcee4aa4ca90be
letunghust <tunglehoctot@gmail.com>
2024-05-10 11:09:59 +0700

    10_5 v1 create chart get total book category

--- src/dashboard/Dashboard.jsx
+++ src/dashboard/Dashboard.jsx
@@ -1,25 +1,44 @@
-import React from "react";
-import { PieChart } from '@mui/x-charts/PieChart';
+import React, { useEffect, useState } from "react";
+import { PieChart } from "@mui/x-charts/PieChart";
 
 const Dashboard = () => {
-  const data = [
-    { id: 0, value: 10, label: 'Sách thiếu nhi' },
-    { id: 1, value: 15, label: 'Sách giáo khoa' },
-    { id: 2, value: 20, label: 'Sách văn học' },
-  ];
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          `${import.meta.env.VITE_BACKEND_URL}/books/getTotalBook`
+        );
+        const data = await response.json();
+        console.log(data);
+        setData(data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  const chartData = data.map((item, index) => ({
+    id: index,
+    value: item.total,
+    label: item._id,
+  }));
 
   return (
     <div>
-        <PieChart
-      series={[
-        {
-          data,
-          highlightScope: { faded: 'global', highlighted: 'item' },
-          faded: { innerRadius: 30, additionalRadius: -30, color: 'gray' },
-        },
-      ]}
-      height={200}
-    />
+      <PieChart
+        series={[
+          {
+            data: chartData,
+            highlightScope: { faded: "global", highlighted: "item" },
+            faded: { innerRadius: 30, additionalRadius: -30, color: "gray" },
+          },
+        ]}
+        height={200}
+      />
     </div>
   );
 };
