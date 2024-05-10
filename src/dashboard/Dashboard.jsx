068f9b15dde9a8c5f6af6a4ea0e2a8078e78c926
letunghust <tunglehoctot@gmail.com>
2024-05-10 11:44:37 +0700

    10_5 v2 get total revenue

--- src/dashboard/Dashboard.jsx
+++ src/dashboard/Dashboard.jsx
@@ -3,21 +3,23 @@ import { PieChart } from "@mui/x-charts/PieChart";
 
 const Dashboard = () => {
   const [data, setData] = useState([]);
+  const [totalRevenue, setTotalRevenue] = useState('');
+
+  // get total book in each category
+  const fetchData = async () => {
+    try {
+      const response = await fetch(
+        `${import.meta.env.VITE_BACKEND_URL}/books/getTotalBook`
+      );
+      const data = await response.json();
+      console.log(data);
+      setData(data);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
 
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch(
-          `${import.meta.env.VITE_BACKEND_URL}/books/getTotalBook`
-        );
-        const data = await response.json();
-        console.log(data);
-        setData(data);
-      } catch (error) {
-        console.error("Error fetching data:", error);
-      }
-    };
-
     fetchData();
   }, []);
 
@@ -26,9 +28,26 @@ const Dashboard = () => {
     value: item.total,
     label: item._id,
   }));
+  // end of get total book in each category
+
+  const fetchDataGetTotalRevenue = async () => {
+    try {
+      const response = await fetch(`${import.meta.env.VITE_BACKEND_URL}/totalRevenue`);
+      const data = await response.json();
+      console.log('revenue', data);
+      setTotalRevenue(data);
+    } catch(error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    fetchDataGetTotalRevenue();
+  }, []);
 
   return (
     <div>
+      <h2> Total revenue: {totalRevenue}$ </h2>
       <PieChart
         series={[
           {
@@ -37,7 +56,7 @@ const Dashboard = () => {
             faded: { innerRadius: 30, additionalRadius: -30, color: "gray" },
           },
         ]}
-        height={200}
+        height={300}
       />
     </div>
   );
